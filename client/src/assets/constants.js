974c965970818056027cf894308a50465a4fdc09
sumonDev13 <sumonmondal1530765@gmail.com>
2024-03-19 19:06:48 +0600

    -wip recommendFu

--- client/src/assets/constants.js
+++ client/src/assets/constants.js
@@ -1,22 +1,5 @@
 import { HiOutlineHashtag, HiOutlineHome, HiOutlinePhotograph } from 'react-icons/hi';
 
-export const genres = [
-  { title: 'Pop', value: 'POP' },
-  { title: 'Hip-Hop', value: 'HIP_HOP_RAP' },
-  { title: 'Dance', value: 'DANCE' },
-  { title: 'Electronic', value: 'ELECTRONIC' },
-  { title: 'Soul', value: 'SOUL_RNB' },
-  { title: 'Alternative', value: 'ALTERNATIVE' },
-  { title: 'Rock', value: 'ROCK' },
-  { title: 'Latin', value: 'LATIN' },
-  { title: 'Film', value: 'FILM_TV' },
-  { title: 'Country', value: 'COUNTRY' },
-  { title: 'Worldwide', value: 'WORLDWIDE' },
-  { title: 'Reggae', value: 'REGGAE_DANCE_HALL' },
-  { title: 'House', value: 'HOUSE' },
-  { title: 'K-Pop', value: 'K_POP' },
-];
-
 export const links = [
   { name: 'Home', to: '/', icon: HiOutlineHome },
   { name: 'Recommend For You', to: '/RecommendForYou', icon: HiOutlinePhotograph },
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,48 +1,42 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { Error, Loader, SongCard } from '../components';
-import { useGetTopChartsQuery } from '../redux/services/shazamCore'; 
-
+import { useDispatch, useSelector } from "react-redux";
+import { Error, Loader, SongCard } from "../components";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Home = () => {
-   
-    const { activeSong, isPlaying } = useSelector((state) => state.player)
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
 
-    const { data, isFetching, error } = useGetTopChartsQuery();
+  const { data, isFetching, error } = useGetTopChartsQuery();
 
-    if (isFetching) return <Loader title="Loading songs..." />;
+  if (isFetching) return <Loader title="Loading songs..." />;
 
-    if (error) return <Error />;
-  
+  if (error) return <Error />;
 
-    return(
-        <div className={styles.container}>
-            <div className={styles.wrapper}>
-                <h2 className='font-bold text-3xl text-white text-left'>
-                    Top Songs 
-                </h2>
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h2 className="font-bold text-3xl text-white text-left">Top Songs</h2>
+      </div>
 
-            <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
-            {data?.map((song, i) => (
-              <SongCard
-                 key={song.key}
-                 song={song}
-                 isPlaying={isPlaying}
-                 activeSong={activeSong}
-                 data={data}
-                 i={i}
-              />
-             ))}
-            </div>
-
-        </div>
-    )
-}
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
 
-const styles ={
-    container:`flex flex-col`,
-    wrapper:`w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10`,
-    selectStyles:`bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5`
-}
+const styles = {
+  container: `flex flex-col`,
+  wrapper: `w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10`,
+  selectStyles: `bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5`,
+};
