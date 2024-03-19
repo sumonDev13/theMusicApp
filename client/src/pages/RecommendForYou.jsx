4d6dd5a7251cfbc3b899997164fd4c166f122459
sumonDev13 <sumonmondal1530765@gmail.com>
2024-03-19 20:10:24 +0600

    complete

--- client/src/pages/RecommendForYou.jsx
+++ client/src/pages/RecommendForYou.jsx
@@ -68,7 +68,7 @@ const RecommendForYou = () => {
   return (
     <div className="flex flex-col">
       <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
-        Around you
+        Recommend For You
       </h2>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
