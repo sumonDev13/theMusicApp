3cfb6edba19f5a8442aeb733ec7b37a29dd3f49e
sumonDev13 <sumonmondal1530765@gmail.com>
2024-03-19 16:15:56 +0600

    refactoring

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,7 +1,7 @@
 import loader from './loader.svg';
-import logo from './logo.svg';
+import logo1 from './logo1.png';
 
 export {
-  logo,
+  logo1,
   loader,
 };
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { RiCloseLine } from 'react-icons/ri';
 import { HiOutlineMenu } from "react-icons/hi";
-import { logo } from '../assets';
+import { logo1 } from '../assets';
 import { links } from '../assets/constants'
 
 
@@ -28,7 +28,7 @@ const Sidebar = () => {
   return(
     <>
      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
-      <img src={logo} alt='logo' className="w-full h-14 object-contain"/>
+      <img src={logo1} alt='logo' className="w-full h-14 object-contain"/>
       <NavLinks />
      </div>
 
@@ -47,7 +47,7 @@ const Sidebar = () => {
 
       <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] 
          backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
-        <img src={logo} alt='logo' className="w-full h-14 object-contain"/>
+        <img src={logo1} alt='logo' className="w-full h-14 object-contain"/>
        <NavLinks 
         handleClick={() => setMobileMenuOpen(false)}
        />
